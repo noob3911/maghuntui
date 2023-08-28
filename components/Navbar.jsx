3edbe019c184f89f62171535b83d20fe5782cce7
Satish <satish.p@ensologic.com>
2023-08-28 20:00:39 +0530

    menu navbar changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaMagnet } from "react-icons/fa";
 import Image from "next/image";
 import Logo from "../public/Maghunt.png";
 import { navlinks } from "@/constants";
 import { BiMenuAltRight } from "react-icons/bi";
 import { CgMagnet } from "react-icons/cg";
+
 const Navbar = () => {
+   const [open, setOpen] = useState(false);
+
    return (
       <header className="padding-x py-2 fixed z-10 bg-blue-600 w-full">
          <nav className=" flex bg-blue-600 justify-between items-center max-container font-palanquin">
             <a href="/">
                <div className="flex items-center justify-end gap-[0.20rem]">
-                  <CgMagnet className="text-white"  size={32}/>
+                  <CgMagnet className="text-white" size={32} />
                   <h2 className="font-palanquin text-3xl text-white">Maghunt</h2>
                </div>
             </a>
@@ -25,7 +28,24 @@ const Navbar = () => {
                ))}
             </ul>
             <div className="hidden max-lg:block">
-               <BiMenuAltRight className="text-white" size={30} />
+               <BiMenuAltRight
+                  onClick={() => {
+                     setOpen(!open);
+                  }}
+                  className="text-white"
+                  size={30}
+               />
+               {open ? (
+                  <div className="flex p-6 bg-blue-500 absolute z-20 top-[3.9rem] right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar">
+                     <ul className="list-none flex justify-end items-start flex-1 flex-col">
+                        {navlinks.map((item) => (
+                           <li className="font-pressfont font-medium cursor-pointer text-white text-[16px] text-dimWhite mb-4">
+                              <a href={item.href}>{item.label}</a>
+                           </li>
+                        ))}
+                     </ul>
+                  </div>
+               ) : null}
             </div>
          </nav>
       </header>
