9ec1c91eb992a9a2243e48bf315e63b5dddf1675
Satish Prajapati <72158548+satishpwebdev@users.noreply.github.com>
2025-01-02 21:34:46 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -9,5 +9,7 @@ export const navlinks = [
 
 // export const url ="https://maghunt.cyclic.cloud/"
 //export const url ="https://magurlhunt.onrender.com"
-export const url = "https://magurlhunt.cloud-station.app"
+// export const url = "https://magurlhunt.cloud-station.app"
+export const url = "https://salty-brigida-learngitto-48d30a28.koyeb.app"
+
 export const localurl ="http://localhost:6000/"
